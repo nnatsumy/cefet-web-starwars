2f8ebd92035995522b0da0b32ff64e9c00cd1127
natsumyiass@gmail.com <natsumyiass@gmail.com>
2018-09-26 22:49:24 -0300

    exercicio 3 quase completo

--- starwars.js
+++ starwars.js
@@ -5,10 +5,9 @@
 //  - A introdução de cada filme, quando ele for clicado
 function adiciona(ep){
 
-	console.log(ep);
 	let title = ep.title;
 	let listaTitulo = document.getElementById('titles');
-	listaTitulo.innerHTML += '<li id="film" data-url-episodio="'+ep.url+'">Episodio '+ep.episode_id+': '+title+'</li>'; 
+	listaTitulo.innerHTML += '<li class="film" data-url-episodio="'+ep.url+'">Episodio '+ep.episode_id+': '+title+'</li>'; 
 	
 }
 
@@ -17,31 +16,32 @@ $.ajax({
   dataType: 'json',
   success: function(resposta) {
     // escreve a resposta no console (para ver o que foi recebido)
-    console.log(resposta);
     // faz o que quiser fazer com a resposta...
     resposta.results.forEach(adiciona);
     // ...
+	let list = document.getElementsByClassName('film');
+
+	for(let element of list){
+		
+		element.addEventListener('click',mudaIntro);
+	}
   }
 });
 
 function mudaIntro(e){
 
+	let el = e.currentTarget.getAttribute('data-url-episodio');
+	console.log(e.currentTarget.innerHTML);
 	$.ajax({
-	  url: 'https://swapi.co/api/films/',
+	  url: el,
 	  dataType: 'json',
 	  success: function(resposta) {
-		// escreve a resposta no console (para ver o que foi recebido)
-		console.log(resposta);
-		// faz o que quiser fazer com a resposta...
-		resposta.results.forEach(adiciona);
-		// ...
+		let intro = document.getElementsByClassName('reading-animation')[0];
+		intro.innerHTML = resposta.opening_crawl;
+		  
 	  }
 	});
 }
 
-let list = document.getElementsById('film');
 
-for(let element of list){
-	
-	element.addEventListener('click',mudaIntro);
-}
+
